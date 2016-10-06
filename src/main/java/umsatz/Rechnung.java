0851db718c40f498eb3252ff86a45c21ab009f9f
Andreas Hoerster <andreas-hoerster@web.de>
2016-10-06 15:07:03 +0200

    Instance Variables and Constructor with array added

--- src/main/java/umsatz/Rechnung.java
+++ src/main/java/umsatz/Rechnung.java
@@ -1,7 +1,19 @@
 package umsatz;
-
 /**
  * Created by andi on 06.10.16.
  */
 public class Rechnung {
+    private int Rechnungsnummer;
+    private int initialeKapazitaet;
+    private int cursor;
+    private Position positionen[];
+
+    public Rechnung(int rechnungsnummer, int initialeKapazitaet, int cursor) {
+        Rechnungsnummer = rechnungsnummer;
+        this.initialeKapazitaet = initialeKapazitaet;
+        this.cursor = cursor;
+        this.positionen = new Position[initialeKapazitaet];
+    }
+
+
 }
