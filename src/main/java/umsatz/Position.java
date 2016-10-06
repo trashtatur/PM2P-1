eafab1c85e1b55fc47507b7b2e78cfca45b6565b
Wurm3 <pjgrages@gmail.com>
2016-10-06 14:54:03 +0200

    The class Position needs GeldBetrag to function properbly. If you change any interfaces the you should notificate the author
    Your God and Master
    Philipp the Wurm
    
    i hate you

---
+++ src/main/java/umsatz/Position.java
@@ -0,0 +1,30 @@
+package umsatz;
+
+/**
+ * Created by the Creator, the great and only King, Prince and God, holy ghost and Master of every mortal beeing Philipp on 06.10.2016.
+ */
+public class Position {
+    private GeldBetrag geldBetrag;
+    private String gerichtOderGetraenk;
+
+    public Position(int cent,int euro, String gerichtOderGetraenk){
+        this.gerichtOderGetraenk = gerichtOderGetraenk;
+        geldBetrag = new GeldBetrag(cent,euro);
+    }
+
+    public String getPrice(){
+        return geldBetrag.toString();
+    }
+
+    public String getGerichtOderGetraenk(){
+        return gerichtOderGetraenk;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return gerichtOderGetraenk != null ? gerichtOderGetraenk.equals(position.gerichtOderGetraenk) : position.gerichtOderGetraenk == null;
+    }
+}
