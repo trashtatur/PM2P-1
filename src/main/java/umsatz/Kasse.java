5f3470d06f79503b23928d6026b14d944fa4831b
Wurm3 <pjgrages@gmail.com>
2016-10-06 15:18:38 +0200

    *Added contructor and add mehtod to the class Kasse

--- src/main/java/umsatz/Kasse.java
+++ src/main/java/umsatz/Kasse.java
@@ -1,5 +1,16 @@
 package umsatz;
 
 public class Kasse {
+    private Array<Rechnung> rechnungArray;
+    private int count = 0;
+
+    public Kasse(int init){
+        rechnungArray = new Array<Rechnung>(init);
+    }
+
+    public void addRechnung(Rechnung rechnung){
+        rechnungArray[count] = rechnung;
+        ++count;
+    }
 
 }
