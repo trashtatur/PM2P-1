927d206b6132be6f14b8ee75951c8b3bc66b250a
Wurm3 <pjgrages@gmail.com>
2016-10-08 12:14:25 +0200

    Ich hasse dich so so sehr

--- src/main/java/umsatz/Kasse.java
+++ src/main/java/umsatz/Kasse.java
@@ -4,23 +4,29 @@ public class Kasse {
     private Rechnung[] rechnungArray;
     private int count = 0;
 
-    public Kasse(int init){
+    public Kasse(int init) {
         rechnungArray = new Rechnung[init];
     }
 
-    public void addRechnung(Rechnung rechnung){
+    public void addRechnung(Rechnung rechnung) {
         rechnungArray[count] = rechnung;
         ++count;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String temp = "";
-        for(Rechnung rech : rechnungArray){
-            temp += rech.toString()  + "|||\n";
+        for (Rechnung rech : rechnungArray) {
+            temp += rech.toString() + "|||\n";
         }
         return temp;
     }
 
+    public String kassenStand() {
+        int betrag;
+        for (Rechnung rechnung : rechnungArray) {
+            rechnung.rechnungsS
 
+        }
+    }
 }
