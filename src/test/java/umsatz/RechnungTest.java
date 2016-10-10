9a4fd6eb43989120ef7a04d6b48ea00b35f26e9f
trashtatur <andreas-hoerster@web.de>
2016-10-10 17:47:05 +0200

    Added test Classes for Kasse and Rechnung

---
+++ src/test/java/umsatz/RechnungTest.java
@@ -0,0 +1,24 @@
+package umsatz;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Andi on 10.10.2016.
+ */
+public class RechnungTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void add() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void rechnungsSumme() throws Exception {
+
+    }
+
+}
