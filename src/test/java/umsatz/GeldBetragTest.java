7abf278f373552e8497638d44285bdb3b7fcbc49
Wurm3 <pjgrages@gmail.com>
2016-10-15 12:24:51 +0200

    Added highCentValueTest Added negaiveValueTest

--- src/test/java/umsatz/GeldBetragTest.java
+++ src/test/java/umsatz/GeldBetragTest.java
@@ -6,9 +6,34 @@ import static org.junit.Assert.*;
  * Created by Philipp on 06.10.2016.
  */
 public class GeldBetragTest {
-    @org.junit.Test
-    public void add() throws Exception {
+    int highCentValue;
+    int convertedEuro;
+    int temp;
+
+
+    @before
+    public void init() {
+        highCentValue = 1567;
+        temp = highCentValue;
+        convertedEuro = 0;
+        while (highCentValue >= 100) {
+            highCentValue -=100;
+            ++convertedEuro;
+        }
+    }
+
+    @Test
+    public void testHighCentValue(){
+        betrag = new GeldBetrag(temp);
+        assertEquals(highCentValue, betrag.getCent());
+        assertEquals(convertedEuro, betrag.getEuro());
+    }
+
+    @Test
+    public void testNegative(){
 
     }
+
+
 
 }
