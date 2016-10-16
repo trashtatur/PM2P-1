e8e11a4aae742bb40dda0e0d896852b474c5381d
Wurm3 <pjgrages@gmail.com>
2016-10-16 13:35:44 +0200

    Finished Add Test for Kasse. Test fails because of a NullPointerExxception in reechnung.rechnungssumme

--- src/test/java/umsatz/KasseTest.java
+++ src/test/java/umsatz/KasseTest.java
@@ -1,19 +1,38 @@
 package umsatz;
 
+import umsatz.Rechnung;
 import static org.junit.Assert.*;
 
 /**
  * Created by Andi on 10.10.2016.
  */
 public class KasseTest {
+    private int anzahl = 5;
+    private Rechnung[] rechnungen = new Rechnung[anzahl];
+    private int betrag = 10000;
+
     @org.junit.Before
     public void setUp() throws Exception {
-
+        int euro = 0;
+        int cent = betrag;
+        while(cent >= 100){
+            ++euro;
+            cent-=100;
+        }
+        for(int i = 0; i < anzahl; ++i){
+            rechnungen[i] = new Rechnung(i);
+            rechnungen[i].add(new Position(cent,euro,"ApfelSaft"));
+        }
+        betrag = betrag*anzahl;
     }
 
     @org.junit.Test
     public void addRechnung() throws Exception {
-
+        Kasse kasse = new Kasse(anzahl);
+        for(int i = 0; i < anzahl; ++i){
+            kasse.addRechnung(rechnungen[i]);
+        }
+        assertEquals(betrag, kasse.kassenStand().getCent()+kasse.kassenStand().getEuro()*100);
     }
 
     @org.junit.Test
