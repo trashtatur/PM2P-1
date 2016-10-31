a0fb865facda68a6fdcb8554f2eb21247f1a9a56
trashtatur <andreas-hoerster@web.de>
2016-10-31 01:33:58 +0100

    Solved the problem of not creating correct Objects of GeldBetrag by removing formatMoney and instead adding formating statements in constructors according to parameters given

--- src/main/java/umsatz/GeldBetrag.java
+++ src/main/java/umsatz/GeldBetrag.java
@@ -4,29 +4,9 @@ package umsatz;
  * Created by andi on 06.10.16.
  */
 public class GeldBetrag {
-	private int cent;
+	private int money;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + cent;
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		GeldBetrag other = (GeldBetrag) obj;
-		if (cent != other.cent)
-			return false;
-		return true;
-	}
+
 
     public GeldBetrag() {
         this(0,0);
@@ -37,32 +17,45 @@ public class GeldBetrag {
             throw new IllegalArgumentException();
 
         }
-        formatMoney(cent,euro);
+        this.money = cent + euro*100;
     }
 
     public GeldBetrag(int cent){
         if(cent < 0){
             throw new IllegalArgumentException();
         }
-        formatMoney(cent, 0);
-    }
-
-    /**
-     * Converts 100 cents to 1  euro. After cent < 100 the whhile loop stops. Then sets the Object Parameter to the correct value
-     * @param cent expects a value as cent. Can be more then 100.
-     * @param euro expects a value as Euro.
-     */
-    private void formatMoney(int cent, int euro) {
-        this.cent = cent + euro*100;
+        this.money=cent*100;
     }
 
 
     public GeldBetrag add(GeldBetrag betrag){
-        this.cent += betrag.cent;
+        this.money += betrag.money;
         return this;
     }
 
     public String toString() {
-        return cent%100 == 0 ? cent/100 + " Euro" : cent/100 + "," + cent%100 + " Euro";
+        return money%100 == 0 ? money/100 + " Euro" : money/100 + "," + money%100 + " Euro";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + money;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GeldBetrag other = (GeldBetrag) obj;
+        if (money != other.money)
+            return false;
+        return true;
     }
 }
